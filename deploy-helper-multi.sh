798fdf9361722dbc234c67ab500c47fadf0224ea
Marek Roszko <mark.roszko@gmail.com>
2024-05-27 16:24:47 -0400

    Multi-arch experiment

---
+++ deploy-helper-multi.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+REAL_RUN=false
+
+while getopts 'i:a:p' opt
+do
+    case "${opt}" in
+        i) IMAGE_BASE_NAME=${OPTARG};;
+        a) ARCHES+=("$OPTARG");;
+        p) REAL_RUN=true;;
+
+        :) usage 1 "-$OPTARG requires an argument" ;;
+        ?) usage 1 "Unknown option '$opt'" ;;
+    esac
+done
+
+shift $((OPTIND -1))
+
+if [[ -z "$IMAGE_BASE_NAME" ]]; then
+    echo "-i arg required."
+    exit;
+fi
+
+MANIFEST_SOURCES=""
+MANIFEST_IMAGE=""
+for arch in "${ARCHES[@]}"; do
+    while read -r MANIFESTTAG && read -r ARCHTAG && read -r GLIMAGE
+    do
+        echo "Read tag: $ARCHTAG image: $GLIMAGE Arch:$arch"
+
+        NEW_IMAGE="$IMAGE_BASE_NAME:$ARCHTAG"
+        if [[ -z "$MANIFEST_IMAGE" ]]; then
+            MANIFEST_IMAGE="$IMAGE_BASE_NAME:$MANIFESTTAG"
+        fi
+
+        echo "Creating tagged image: $NEW_IMAGE"
+        if [ "$REAL_RUN" = true ] ; then
+            docker pull $GLIMAGE
+            docker tag $GLIMAGE $NEW_IMAGE
+            docker push $NEW_IMAGE
+        else
+            echo "Pulling: $GLIMAGE"
+            echo "Tagging: $GLIMAGE to: $NEW_IMAGE"
+            echo "Pushing: $NEW_IMAGE"
+        fi
+
+        MANIFEST_SOURCES+="$NEW_IMAGE "
+    done < "$arch-docker_tags.txt"
+
+    if [ "$REAL_RUN" = true ] ; then
+        docker buildx imagetools create -t ${MANIFEST_IMAGE} ${MANIFEST_SOURCES}
+    else
+        echo "Creating manifest: ${MANIFEST_IMAGE} from ${MANIFEST_SOURCES}"
+    fi
+done
--- tag-helper.sh
+++ tag-helper.sh
@@ -5,7 +5,7 @@ unset -v TAG_BASE_NAME
 
 PUSH_IMAGE=false
 
-while getopts 'c:t:b:r:i:p' opt
+while getopts 'a:c:t:b:r:i:p' opt
 do
     case "${opt}" in
         c) ORIG_CONTAINER=${OPTARG};;
@@ -14,7 +14,8 @@ do
         r) RELEASE_TAG=${OPTARG};;
         i) IMAGE_BASE_NAME=${OPTARG};;
         p) PUSH_IMAGE=true;;
-        
+        a) ARCH=${OPTARG};;
+
         :) usage 1 "-$OPTARG requires an argument" ;;
         ?) usage 1 "Unknown option '$opt'" ;;
     esac
@@ -42,9 +43,9 @@ fi
 # 'monthly' - will append the current YYYYMM to the container base name
 # 'daily' - will simply return back the current base name
 # 'release' - will r
-# 
+#
 # BUILD_TAG - required for release
-# 
+#
 
 > docker_tags.txt
 # Check if the KI_BUILD_TYPE variable is "monthly"
@@ -69,13 +70,13 @@ if [[ "$BUILD_TYPE" == "monthly" ]]; then
         docker push $CONTAINER_IMAGE
     fi
 elif [[ "$BUILD_TYPE" == "release" ]]; then
-    if [[ $RELEASE_TAG =~ ([0-9]+)\.([0-9]+)\.([0-9]+) ]]; 
+    if [[ $RELEASE_TAG =~ ([0-9]+)\.([0-9]+)\.([0-9]+) ]];
     then
         FULL_VERSION=${BASH_REMATCH[0]}
         MAJOR_MINOR_VERSION="${BASH_REMATCH[1]}.${BASH_REMATCH[2]}"
     else
         echo "-r is required for release builds and must be a properly formatted version MAJOR.MINOR.PATCH."
-        exit;  
+        exit;
     fi
 
     # Print the month and year string
@@ -83,7 +84,7 @@ elif [[ "$BUILD_TYPE" == "release" ]]; then
     CONTAINER_IMAGE="$IMAGE_BASE_NAME:$CONTAINER_TAG"
     echo $CONTAINER_TAG >> docker_tags.txt
     echo $CONTAINER_IMAGE >> docker_tags.txt
-    
+
     if [ "$PUSH_IMAGE" = true ] ; then
         docker tag $ORIG_CONTAINER $CONTAINER_IMAGE
         docker push $CONTAINER_IMAGE
@@ -93,7 +94,7 @@ elif [[ "$BUILD_TYPE" == "release" ]]; then
     CONTAINER_IMAGE="$IMAGE_BASE_NAME:$CONTAINER_TAG"
     echo $CONTAINER_TAG >> docker_tags.txt
     echo $CONTAINER_IMAGE >> docker_tags.txt
-    
+
     if [ "$PUSH_IMAGE" = true ] ; then
         docker tag $ORIG_CONTAINER $CONTAINER_IMAGE
         docker push $CONTAINER_IMAGE
@@ -105,10 +106,23 @@ elif [[ "$BUILD_TYPE" == "daily" ]]; then
     fi
 
     CONTAINER_TAG="${TAG_BASE_NAME}"
+    CONTAINER_TAG_ORIG=$CONTAINER_TAG
+    if [ "$ARCH" ]; then
+        CONTAINER_TAG="${ARCH}-${TAG_BASE_NAME}"
+    fi
+
     CONTAINER_IMAGE="$IMAGE_BASE_NAME:$CONTAINER_TAG"
 
-    echo $CONTAINER_TAG >> docker_tags.txt
-    echo $CONTAINER_IMAGE >> docker_tags.txt
+    TAGS_FILENAME="docker_tags.txt"
+    if [ $ARCH ]; then
+        TAGS_FILENAME="$ARCH-docker_tags.txt"
+    fi
+
+    if [ "$ARCH" ]; then
+        echo $CONTAINER_TAG_ORIG >> "${TAGS_FILENAME}"
+    fi
+    echo $CONTAINER_TAG >> "${TAGS_FILENAME}"
+    echo $CONTAINER_IMAGE >> "${TAGS_FILENAME}"
 
     if [ "$PUSH_IMAGE" = true ] ; then
         docker tag $ORIG_CONTAINER $CONTAINER_IMAGE
